e74190349fb40e3edd915687b9ee9061e9c5da90
vladimirPutin228z <qwertyutinfcxcove@gmail.com>
2024-09-23 16:44:48 +0300

    Create ex 1, 2, 3 in git.py

---
+++ ex 1, 2, 3 in git.py
@@ -0,0 +1,32 @@
+# 1
+integer_var=42
+float_var=2,3
+string_var="крутой текст прям мега круто"
+list_var=[1,2,3,4,]
+tuple_var=(92,93)
+dict_var={"name": "Ivan", "age": 23 }
+set_var={1,2,3}
+bool_var = True
+print(integer_var)
+print(float_var)
+print(string_var)
+print(list_var)
+print(tuple_var)
+print(dict_var)
+print(set_var)
+print(bool_var)
+# 2
+name = "Marat"
+age = 18
+average_grade = "2,5"
+favorite_movies = ["The Matrix","Interstellar", "inception"]
+personal_info =("Rozentur", 138, 2,5)
+skills = {"игра в бравл": "продвинутый","пунктуальность":"Cредний","нормис";"начальный"}
+visited_countres = ["Беларусь","Англия", "Германия"]
+has_drive_license = False
+string_var ="Intel Core i5 14500"
+integer_var = 32
+# 3
+speed = 600 Mbps
+list =[ "string_var","integer_var", "speed"]
+print(list)
